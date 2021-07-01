55a19d24c83c35ca62e061f5c52b7133e2283e9d
Brandon Keefer <happysolucki@outlook.com>
2021-07-01 13:57:43 -0500

    Refactored functions

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,19 +1,21 @@
-const checkNation = function determineFavNation(nation) {
+const checkNation = function determineFavNation() {
+  let nation = prompt("Which nation are you a part of? Water, Earth, Fire, or Air?").toLowerCase();
   if (nation === 'water') return 'Water Tribe';
   if (nation === 'earth') return 'Earth Kingom';
   if (nation === 'fire') return 'Fire Nation';
   if (nation === 'air') return 'Air Nomads';
+  return checkNation();
 }
 
 const checkFavChar = function determineFavoriteCharacter() {
-  let favNation = prompt("Which nation are you a part of? Water, Earth, Fire, or Air?").toLowerCase();
-  let favChar = prompt(`Who's your favorite character from the ${checkNation(favNation)}?`);
+  let favNation = checkNation();
+  let favChar = prompt(`Who's your favorite character from the ${favNation}?`);
   let bar = document.querySelector('.sidebar')
   let userFavs = document.createElement('div');
   let nation = document.createElement("h4");
   let character = document.createElement("h4");
-  nation.innerText = `${checkNation(favNation)} Citizen`;
-  character.innerText = `${favChar} Fan`;
+  nation.innerText = `${favNation} Citizen`;
+  character.innerText = `${favChar[0].toUpperCase() + favChar.substring(1)} Fan`;
   userFavs.appendChild(nation);
   userFavs.appendChild(character);
   bar.appendChild(userFavs);
